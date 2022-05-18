207d2b497a7dd9abd7f183a4143282099d45fb96
Josh Levy <joshaaronlevy@gmail.com>
2022-05-18 08:28:16 -0600

    Updated base branding and initial repo setup

---
+++ assets/theme/components/dialog/dialogTitle.js
@@ -0,0 +1,18 @@
+// NextJS Material Dashboard base styles
+import typography from "/assets/theme/base/typography";
+
+// NextJS Material Dashboard helper functions
+import pxToRem from "/assets/theme/functions/pxToRem";
+
+const { size } = typography;
+
+const dialogTitle = {
+  styleOverrides: {
+    root: {
+      padding: pxToRem(16),
+      fontSize: size.xl,
+    },
+  },
+};
+
+export default dialogTitle;
---
+++ assets/theme/components/form/formLabel.js
@@ -0,0 +1,14 @@
+// NextJS Material Dashboard base styles
+import colors from "/assets/theme/base/colors";
+
+const { text } = colors;
+
+const formLabel = {
+  styleOverrides: {
+    root: {
+      color: text.main,
+    },
+  },
+};
+
+export default formLabel;
